4f4400a08103d855249bac0a75032377b5884dae
LuckyShmucky <christianluciano567@gmail.com>
2022-03-09 19:42:43 -0800

    made changes to 404 page

---
+++ views/404.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function fourOFour(props){
+return (
+    <div>
+        <p>You've reached the wrong page</p>
+        <a href='/breads'>Go home</a>
+    </div>
+)
+}
+
+module.exports = fourOFour
