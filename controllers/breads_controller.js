c49feaf3ac3e86b4b201f89f9d578ad1a77bd653
LuckyShmucky <christianluciano567@gmail.com>
2022-03-09 21:42:28 -0800

    added post feature, and some navigation

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,6 +13,11 @@ breads.get('/', (req, res) => {
     )
 })
 
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('new')
+})
+
 // SHOW
 // breads.get('/:arrayIndex', (req, res) => {
 //    const pickBread = Bread[Number(req.params.arrayIndex)]
@@ -43,4 +48,18 @@ breads.get('/:arrayIndex', (req, res) => {
     }
   })
 
+  // CREATE
+breads.post('/', (req, res) => {
+ const hasImage = req.body.image;
+ if(!hasImage){
+     req.body.Image = 'https://images.unsplash.com/photo-1598373182133-52452f7691ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+ }
+    if(req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
+  })
 module.exports = breads
--- views/index.jsx
+++ views/index.jsx
@@ -10,6 +10,7 @@ function Index ({breads, title}) {
       <Default title={title}>
        {/* <h2>Index Page</h2>
         <p>I have {breads[0].name} bread!</p> */}
+
         <ul>
         {breads.map((bread, index) =>{
             return (
@@ -19,6 +20,10 @@ function Index ({breads, title}) {
             )
         })}
         </ul>
+
+        <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
       </Default>
     )
 }
